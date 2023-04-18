d005c90caa61955b84b19b07df317bee1e43f814
Chicory Roth <chicoryroth@gmail.com>
2023-04-18 15:12:41 -0700

    added simple location finder

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -4,12 +4,32 @@ import { Image } from 'react-native';
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
 import AWSImageSrc from '../../components/AWSImage';
+import * as Location from 'expo-location';
+import { useEffect, useState } from 'react';
 
 
 export default function TabTwoScreen() {
+  const [location, setLocation] = useState<Location.LocationObject>();
+  const [errorMsg, setErrorMsg] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Two</Text>
+      <Text>Lat: {JSON.stringify(location?.coords.latitude)} Lon: {JSON.stringify(location?.coords.longitude)}</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/two.tsx" />
       <Image style={{width: "50%", height: "50%"}}source={{uri: AWSImageSrc()}}/>
