243e373738a4ce457d1d055e2438b77e98752d74
Chicory Roth <chicoryroth@gmail.com>
2023-05-04 11:36:43 -0700

    file uploads to firebase now, needs tweaking still

--- app/(tabs)/post.tsx
+++ app/(tabs)/post.tsx
@@ -1,10 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { Button, Image, View, Platform } from 'react-native';
+import React, { useState } from 'react';
+import { Button, Image, View } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage"
+import {storage } from "../../firebase"
+import uuid from 'react-native-uuid';
+import * as ImageManipulator from 'expo-image-manipulator';
+import { ImageResult } from 'expo-image-manipulator';
 
 
 export default function Post() {
-    const [image, setImage] = useState("");
+    const [imageURI, setImageURI] = useState("");
+    const [image, setImage] = useState<ImageResult>();
     
     const pickImage = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
@@ -12,16 +18,33 @@ export default function Post() {
           allowsEditing: true,
           aspect: [4, 3],
           quality: 1,
+          base64: true, // Remove?
         });
     
         if (!result.canceled) {
-          setImage(result.assets[0].uri);
+          setImageURI(result.assets[0].uri);
+          setImage(await ImageManipulator.manipulateAsync(result.assets[0].uri, [{resize: {width: 500}}], {compress: 1})) 
         }
       };
+
+    const uploadImage = async () => {
+      const imageID = uuid.v1()
+      
+      if(image){
+        const response = await fetch(image.uri);
+        const blobFile = await response.blob();
+        const imageRef = ref(storage, `images/${imageID}`);
+        const result = await uploadBytes(imageRef, blobFile);
+        //TODO: add "uploading" icon while it's uploading, then go to diff page or change page when done.
+      }     
+    }
+
     return(
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Button title="Pick an image from camera roll" onPress={pickImage} />
-            {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+            {imageURI && <Image source={{ uri: imageURI }} style={{ width: 200, height: 200 }} />}
+            {image && <Button title="Upload image" onPress={uploadImage} />}
+            
     </View>
     )
 }
--- firebase.ts
+++ firebase.ts
@@ -1,6 +1,7 @@
 
 import * as firebase from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { getStorage} from "firebase/storage"
 import {REACT_APP_API_KEY, REACT_APP_AUTH_DOMAIN,
   REACT_APP_PROJECT_ID, REACT_APP_STORAGE_BUCKET,
   REACT_APP_MESSAGING_SENDER_ID, 
@@ -25,4 +26,5 @@ if(firebase.getApps().length === 0){
 }
 
 const auth = getAuth(app);
-export { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword };
+const storage = getStorage(app);
+export { auth, storage, createUserWithEmailAndPassword, signInWithEmailAndPassword };
