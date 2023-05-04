0ece986dbcdb3030bc3d87a72ce124abcfd1796d
Chicory Roth <chicoryroth@gmail.com>
2023-05-03 21:02:32 -0700

    added image picker

--- app/(tabs)/post.tsx
+++ app/(tabs)/post.tsx
@@ -1,7 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import { Button, Image, View, Platform } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+
 
 export default function Post() {
+    const [image, setImage] = useState("");
+    
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.All,
+          allowsEditing: true,
+          aspect: [4, 3],
+          quality: 1,
+        });
+    
+        if (!result.canceled) {
+          setImage(result.assets[0].uri);
+        }
+      };
     return(
-        <>
-        </>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Button title="Pick an image from camera roll" onPress={pickImage} />
+            {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+    </View>
     )
 }
