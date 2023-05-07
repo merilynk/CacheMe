7885a91b71e3ec3979a27fec81a5d9163c4e808c
Merilyn Kuo <merilynk02@gmail.com>
2023-05-06 23:02:18 -0700

    dynamic post

--- app/(tabs)/feed.tsx
+++ app/(tabs)/feed.tsx
@@ -1,19 +1,57 @@
-import React from 'react'
+// Built in modules
+import React, { Component } from 'react'
 import { SafeAreaView, StyleSheet, Text, View, ScrollView } from "react-native"
-import PostPreview from '../../components/posts'
+import { LayoutAnimation, RefreshControl } from 'react-native';
+
+// Components
+import PostPreview from '../../components/Post'
 import Header from '../../components/Header'
 
+// Firebase
+import { auth, db } from '../../firebase';
+import { GeoPoint, Timestamp } from 'firebase/firestore';
 
-export default function feed() {
-    return(
-        <SafeAreaView>
-            <ScrollView stickyHeaderIndices={[0]} style={styles.container}>
-                <Header/>
-                <PostPreview></PostPreview>
-            </ScrollView>
-        </SafeAreaView>
-    )
+
+// Set the default number of posts to load for each pagiation
+const PAGE_SIZE = 20;
+
+export default class Feed extends Component {
+    state = {
+        loading: false,
+        posts: [],
+        data: {},
+    };
+
+
+    render () {
+        LayoutAnimation.easeInEaseOut();
+        return(
+            <SafeAreaView>
+                <ScrollView stickyHeaderIndices={[0]} style={styles.container}>
+                    <Header/>
+                    <PostPreview captionText={"hello"} 
+                        uid={"mgEEj5Bn8PKhEtj2X6dl"} 
+                        image={"https://i0.wp.com/yalewanders.com/wp-content/uploads/2020/01/BAA926E3-465D-4A3C-AE4A-B0984239CFB3.jpg?fit=4032%2C3024&ssl=1"}
+                        numComments={200}
+                        numLikes={400}
+                        location={new GeoPoint(36.99, 122.05)}
+                        timePosted={new Timestamp(800, 894023)}/>
+                </ScrollView>
+            </SafeAreaView>
+        )
+    }
 }
+
+// export default function feed() {
+//     return(
+//         <SafeAreaView>
+//             <ScrollView stickyHeaderIndices={[0]} style={styles.container}>
+//                 <Header/>
+//                 <PostPreview></PostPreview>
+//             </ScrollView>
+//         </SafeAreaView>
+//     )
+// }
 
 const styles = StyleSheet.create({
     container: {
---
+++ components/Post.tsx
@@ -0,0 +1,228 @@
+import React, {useState, useEffect} from 'react'
+//import ProfileInfo from './ProfileInfor'
+import { useWindowDimensions, View, Image, StyleSheet, TouchableOpacity, Text} from 'react-native'
+import { AntDesign } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import RegularText from './Texts/regularText';
+import BigText from './Texts/bigText';
+import { LinearGradient } from 'expo-linear-gradient';
+import moment from 'moment';
+
+import { collection, addDoc, setDoc, doc, getDoc, GeoPoint, Timestamp } from "firebase/firestore";
+import { db, auth } from '../firebase';;
+
+type PostProps = {
+    captionText: string,
+    uid: string, 
+    image: string,
+    numComments: number,
+    numLikes: number,
+    location: GeoPoint,
+    timePosted: Timestamp,
+}
+const PostPreview = (props: PostProps) => {
+
+    const {width} = useWindowDimensions()
+    const [isLiked, setIsLiked] = useState(false);
+    const [isComment, setIsComment] = useState(false);
+    const [likeCount, setLikeCount] = useState(props.numLikes);
+    const [commentCount, setCommentCount] = useState(props.numComments);
+    const [poster, setPoster] = useState("");
+
+    const getPoster = async () => {
+        const docSnap = await getDoc(doc(db, "user", props.uid));
+        if (docSnap.exists()) {
+            setPoster(docSnap.data().username)
+            return docSnap.data().username;
+        }
+        else {
+            setPoster("InvalidUser");
+            return "InvalidUser";
+        }
+    }
+
+    useEffect( () => {
+        getPoster();
+    }, []);
+
+    const toggleLike = () => {
+        if (isLiked) {
+            setLikeCount(likeCount - 1)
+        } else {
+            setLikeCount(likeCount + 1)
+        }
+        setIsLiked(!isLiked);
+    };
+    const toggleComment = () => {
+        if (isComment) {
+            setCommentCount(commentCount - 1)
+        } else {
+            setCommentCount(commentCount + 1)
+        }
+        setIsComment(!isComment);
+    };
+
+    const locatePost = () => {
+        console.log("Post is located at " + props.location);
+    }
+
+    // const ProfileInfo = ( {username} ) => {
+    //     return (
+    //         <View style={styles.container}>
+    //         <Image source = {require("../assets/images/takumi.jpeg")} style={styles.profileImage}/>
+    //         <View style={styles.profileContainer}>
+    //             <RegularText style={styles.name} >{ username }</RegularText>
+    //             <Text style={styles.postTime}>{ moment(timePosted).fromNow() }</Text>
+    //         </View>
+    //       </View>
+    //      );
+    //  }
+
+    // const Caption = ( {text} ) => (
+    //     <View style={styles.text}>
+    //         <RegularText>{text}</RegularText>
+    //     </View>
+    // )
+
+    // const IconBar = () => (
+    //     <View>
+    //         <LinearGradient
+    //             colors={['rgba(44, 218, 157, .45)', 'rgba(4, 67, 137, .45)']}
+    //             start={{ x: 0, y: 0 }}
+    //             end={{ x: 1, y: 1 }}
+    //             style={styles.gradient}>
+    //             <View style={styles.icons}>
+    //                 <TouchableOpacity onPress={toggleLike}>
+    //                     <AntDesign name="heart" size={35} color={isLiked ? '#FF6E6E' : '#FFFFFF'}/>
+    //                 </TouchableOpacity>
+    //                 <BigText style={{ marginLeft: 22}}>{likeCount}</BigText>
+    
+    //                 <TouchableOpacity onPress={toggleComment}>
+    //                     <FontAwesome name="comment" size={35} color="white" style={{marginLeft: 66}}/>
+    //                 </TouchableOpacity>
+    //                 <BigText style={{ marginLeft: 22}}>{commentCount}</BigText>
+    //             </View>
+    //             <View style={styles.icons}>
+    //                 <TouchableOpacity onPress={ locatePost(location) }>
+    //                     <Ionicons name="location-sharp" size={35} color="white" />
+    //                 </TouchableOpacity>
+    //             </View>
+    //         </LinearGradient>
+    //     </View>
+    // )
+    
+
+    return (
+        <View style={styles.outerContainer}>
+            <View style={styles.container}>
+             <Image source = {require("../assets/images/takumi.jpeg")} style={styles.profileImage}/>
+             <View style={styles.profileContainer}>
+                 <RegularText style={styles.name} >{ poster }</RegularText>
+                 <Text style={styles.postTime}>{ moment(props.timePosted).fromNow() }</Text>
+             </View>
+            </View>
+            <View style={styles.postContainer}> 
+                <Image source={{uri: props.image}}
+                style={{
+                    width: width - 30, 
+                    height: width - 30, 
+                    borderRadius: 15,
+                    }}
+                />
+            </View> 
+            <View style={styles.text}>
+                <RegularText>{props.captionText}</RegularText>
+            </View>
+            <View>
+                <LinearGradient
+                    colors={['rgba(44, 218, 157, .45)', 'rgba(4, 67, 137, .45)']}
+                    start={{ x: 0, y: 0 }}
+                    end={{ x: 1, y: 1 }}
+                    style={styles.gradient}>
+                    <View style={styles.icons}>
+                        <TouchableOpacity onPress={toggleLike}>
+                            <AntDesign name="heart" size={35} color={isLiked ? '#FF6E6E' : '#FFFFFF'}/>
+                        </TouchableOpacity>
+                        <BigText style={{ marginLeft: 22}}>{likeCount}</BigText>
+        
+                        <TouchableOpacity onPress={toggleComment}>
+                            <FontAwesome name="comment" size={35} color="white" style={{marginLeft: 66}}/>
+                        </TouchableOpacity>
+                        <BigText style={{ marginLeft: 22}}>{commentCount}</BigText>
+                    </View>
+                    <View style={styles.icons}>
+                        <TouchableOpacity onPress={locatePost}>
+                            <Ionicons name="location-sharp" size={35} color="white" />
+                        </TouchableOpacity>
+                    </View>
+                </LinearGradient>
+            </View>
+        </View>
+    )
+}
+
+
+
+const styles = StyleSheet.create({
+    outerContainer: {
+        transform: [{ scale: 0.97}],
+        flex: 1,
+        flexDirection: 'column',
+        paddingBottom: 0,
+        marginVertical: 0,
+        margin: 5,
+        marginBottom: 0,
+        backgroundColor: "white",
+        borderRadius: 15,
+    },
+    postContainer: {
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    text: {
+        margin: 15,
+        marginLeft: 20,
+        marginRight: 20,
+        justifyContent: "flex-start",
+    },
+    gradient: {
+        flex: 1,
+        paddingTop: 10,
+        paddingBottom: 10,
+        borderBottomLeftRadius: 15,
+        borderBottomRightRadius: 15,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    icons: {
+        flexDirection: "row",
+        marginHorizontal: 20,
+    },
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        margin: 10,
+    },
+    profileContainer: {
+        marginLeft: 8,
+    },
+    profileImage: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+    },
+    name: {
+        fontWeight: "700",
+        fontSize: 16,
+    },
+    postTime: {
+        fontSize: 14,
+        fontWeight: "500",
+    },
+})
+
+
+export default PostPreview
+
