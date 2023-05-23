feea43f5c3808905d25db0e6ac219644da33c87c
Anjalikhadka <85704283+Anjalikhadka@users.noreply.github.com>
2023-05-23 10:00:40 -0700

    Added reply functionality

--- components/PostPageComponents/PostComment.tsx
+++ components/PostPageComponents/PostComment.tsx
@@ -1,6 +1,6 @@
 import { VoiceID } from 'aws-sdk';
-import React from 'react';
-import { View, StyleSheet, Image, Dimensions, Text, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, Image, Dimensions, Text, TouchableOpacity, TextInput } from 'react-native';
 import RegularText from "../Texts/regularText"
 import MiniText from '../Texts/miniText';
 
@@ -8,6 +8,25 @@ const windowWidth = Dimensions.get('screen').width;
 const windowHeight = Dimensions.get('screen').height;
 
 const PostComment = () => {
+  const [showReplyBox, setShowReplyBox] = useState(false);
+  const [replyMessage, setReplyMessage] = useState('');
+  const [postedReply, setPostedReply] = useState('');
+
+  const handleReply = () => {
+    // Handle reply logic here
+    console.log('Reply:', replyMessage);
+    // Display the posted reply
+    setPostedReply(replyMessage);
+    // Clear reply message after sending
+    setReplyMessage('');
+    // Show the reply button again
+    setShowReplyBox(false);
+  };
+
+  const handleShowReplyBox = () => {
+    setShowReplyBox(true);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.topBar}>
@@ -24,10 +43,33 @@ const PostComment = () => {
       <View style={styles.midderBar}>
         <RegularText style={{fontSize: 15}}>This is a really long comment just to see if it overflows over the right or goes down to the bottom</RegularText>
       </View>
-      
-      <TouchableOpacity style={styles.bottomBar}>
-        <RegularText style={{fontSize: 13, marginTop: 10,}}>Reply</RegularText>
-      </TouchableOpacity>
+
+      {!showReplyBox && (
+        <TouchableOpacity style={styles.bottomBar} onPress={handleShowReplyBox}>
+          <RegularText style={{fontSize: 13, marginTop: 10,}}>Reply</RegularText>
+        </TouchableOpacity>
+      )}
+
+      {showReplyBox && (
+        <View style={styles.replyContainer}>
+          <TextInput
+            style={styles.replyInput}
+            placeholder="Type your reply..."
+            value={replyMessage}
+            onChangeText={setReplyMessage}
+          />
+          <TouchableOpacity style={styles.replyButton} onPress={handleReply}>
+            <Text style={styles.replyButtonText}>Send</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+
+      {/* Display the posted reply */}
+      {postedReply !== '' && (
+        <View style={styles.postedReplyContainer}>
+          <RegularText style={styles.postedReplyText}>{postedReply}</RegularText>
+        </View>
+      )}
     </View>
   );
 };
@@ -61,9 +103,42 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingLeft: 45,
   },
+ 
   bottomBar: {
     paddingLeft: 45,
-  }
+  },
+  replyContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 45,
+    paddingTop: 10,
+  },
+  replyInput: {
+    flex: 1,
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#000',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginRight: 10,
+  },
+  replyButton: {
+    backgroundColor: 'blue',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 5,
+  },
+  replyButtonText: {
+    color: '#FFF',
+    fontSize: 14,
+  },
+  postedReplyContainer: {
+    paddingHorizontal: 45,
+    marginTop: 10,
+  },
+  postedReplyText: {
+    fontSize: 15,
+  },
 });
 
 export default PostComment;
