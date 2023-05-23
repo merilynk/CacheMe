1b649a0c495c0cf5248815948ac70dc97184828b
Merilyn Kuo <merilynk02@gmail.com>
2023-05-23 11:36:48 -0700

    map was commented out

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,60 +1,60 @@
-// import MapView from 'react-native-maps';
-// import { StyleSheet, View, ActivityIndicator } from 'react-native';
-// import { PROVIDER_GOOGLE } from 'react-native-maps';
-// import { useEffect, useState } from 'react';
-// import getLocation from '../../helpers/location';
-// import React from 'react';
-// import Location from 'expo-location';
+import MapView from 'react-native-maps';
+import { StyleSheet, View, ActivityIndicator } from 'react-native';
+import { PROVIDER_GOOGLE } from 'react-native-maps';
+import { useEffect, useState } from 'react';
+import getLocation from '../../helpers/location';
+import React from 'react';
+import Location from 'expo-location';
 
 
 
 
-// export default function Map() {
-//   const [location, setLocation] = useState<Location.LocationObject | null | undefined>();
-//   const [loading, setLoading] = useState(true);
+export default function Map() {
+  const [location, setLocation] = useState<Location.LocationObject | null | undefined>();
+  const [loading, setLoading] = useState(true);
 
 
-//   const mapRef = React.createRef();
-//   useEffect(() => {
-//     (async () => {
+  const mapRef = React.createRef();
+  useEffect(() => {
+    (async () => {
 
-//         const loc = await getLocation()
-//         setLocation(loc);
-//         setLoading(false);
-//     })();
-// }, []);
+        const loc = await getLocation()
+        setLocation(loc);
+        setLoading(false);
+    })();
+}, []);
 
-//   if(loading){
-//     return(
-//       <View style={[styles.container, styles.map]}>
-//         <ActivityIndicator size="large" />
-//       </View>
-//     )
-//   }
+  if(loading){
+    return(
+      <View style={[styles.container, styles.map]}>
+        <ActivityIndicator size="large" />
+      </View>
+    )
+  }
 
-//   return (
-//     <View style={styles.container}>
-//       <MapView
-//       style={styles.map}
-//       showsUserLocation={true}
-//       provider={PROVIDER_GOOGLE}
-//       region={{
-//         latitude: location?.coords.latitude as number,
-//         longitude: location?.coords.longitude as number,
-//         latitudeDelta: .28,
-//         longitudeDelta: .28
-//       }}></MapView>
-//     </View>
-//   );
-// }
+  return (
+    <View style={styles.container}>
+      <MapView
+      style={styles.map}
+      showsUserLocation={true}
+      provider={PROVIDER_GOOGLE}
+      region={{
+        latitude: location?.coords.latitude as number,
+        longitude: location?.coords.longitude as number,
+        latitudeDelta: .28,
+        longitudeDelta: .28
+      }}></MapView>
+    </View>
+  );
+}
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//   },
-//   map: {
-//     width: '100%',
-//     height: '100%',
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
