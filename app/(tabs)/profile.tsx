ee2a475214703bda913c31bd964b734e54200de6
Merilyn Kuo <merilynk02@gmail.com>
2023-06-01 13:22:07 -0700

    finished profile basic layout

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, TouchableOpacity, View, Image, Dimensions, ActivityIndicator } from 'react-native'
-import { auth, db, storage } from '../../firebase'
+import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native'
+import { auth, db } from '../../firebase'
 import { useRouter } from 'expo-router';
 import ProfilePicture from "../../components/profile/ProfilePicture"
 import { useEffect, useState } from 'react';
@@ -15,8 +15,6 @@ type UserData = {
   friends: [];
 }
 
-const windowWidth = Dimensions.get('screen').width
-const windowHeight = Dimensions.get('screen').height
 
 
 export default function Home() {
@@ -56,53 +54,112 @@ export default function Home() {
             router.push("/");
           })
           .catch(error => alert(error.message))
-    }
-    const changeProfilePictureID = (newID: string) => {
-      setProfilePictureID(newID);
-    }
+      }
 
-    
-    if(user){
-      return (
-        <View style={styles.container}>
-            {profilePictureID && <ProfilePicture profilePictureID={profilePictureID}/>}
-            <Text>Email: {user.email}</Text>
-            <Text>Name: {user.name}</Text>
-            <Text>Username: {user.username}</Text>
-            {user.__id && <ChangeProfilePicture userID={user.__id} changeProfilePictureID={changeProfilePictureID} />}
-            <TouchableOpacity
-            onPress={handleSignOut}
-            style={styles.button}>
-            <Text style={styles.buttonText}>Sign out</Text>
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity>
+            <Text>Back</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text>Sign Out</Text>
           </TouchableOpacity>
         </View>
-      )
-    }else{
-      <View style={styles.container}>
-        <ActivityIndicator size="large" />
+        <View style={styles.profileContainer}>
+          <Image source={require('../../assets/images/takumi.jpeg')} style={{width: 100, height: 100, borderRadius: 50,}}></Image>
+          <Text>FirstName Lastname</Text>
+          <Text>@username</Text>
+          <View style={styles.profileData}>
+            <View style={styles.data}>
+              <Text>500</Text>
+              <Text>Friends</Text>
+            </View>
+            <View style={styles.data}>
+              <Text>6</Text>
+              <Text>Posts</Text>
+            </View>
+          </View>
+          <TouchableOpacity style={styles.friendButton}><Text>Friend</Text></TouchableOpacity>
+        </View>
       </View>
-    }
-    
+        // <View style={styles.container}>
+        //     <Text>Email: {auth.currentUser?.email}</Text>
+        //   <TouchableOpacity
+        //     onPress={handleSignOut}
+        //     style={styles.button}
+        //   >
+        //     <Text style={styles.buttonText}>Sign out</Text>
+        //   </TouchableOpacity>
+        // </View>
+      )
 }
 
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-    },
-     button: {
-      backgroundColor: '#0782F9',
-      width: '60%',
-      padding: 15,
-      borderRadius: 10,
+      justifyContent: 'flex-start',
+      flexDirection: 'column',
       alignItems: 'center',
-      marginTop: 40,
+      flexWrap: 'wrap',
+      borderColor: 'green',
+      borderWidth: 1,
     },
-    buttonText: {
-      color: 'white',
-      fontWeight: '700',
-      fontSize: 16,
+    header: {
+      // flex: 1,
+      width: "100%",
+      height: 50,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      marginTop: 30,
+      borderColor: 'blue',
+      borderWidth: 1,
     },
+    profileContainer: {
+      // flex: 5,
+      width: '100%',
+      height: 263,
+      alignContent: 'center',
+      alignItems: 'center',
+      flexWrap: 'wrap',
+      borderColor: 'red',
+      borderWidth: 1,
+    },
+    profileData: {
+      flex: 1,
+      width: 164,
+      height: 41,
+      marginTop: 11,
+      justifyContent: 'space-between',
+      flexDirection: 'row',
+      borderColor: 'pink',
+      borderWidth: 1,
+    },
+    data: {
+      flexDirection: 'column',
+      justifyContent: 'center',
+      borderColor: 'purple',
+      borderWidth: 1,
+    },
+    friendButton: {
+      width: 82,
+      height: 25,
+      marginTop: 10,
+      borderColor: 'cyan',
+      borderWidth: 1,
+    }
+    //  button: {
+    //   backgroundColor: '#0782F9',
+    //   width: '60%',
+    //   padding: 15,
+    //   borderRadius: 10,
+    //   alignItems: 'center',
+    //   marginTop: 40,
+    // },
+    // buttonText: {
+    //   color: 'white',
+    //   fontWeight: '700',
+    //   fontSize: 16,
+    // },
   })
   
